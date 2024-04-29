61ae10116a1978dc5dba87d0f65de91ab723edd1
dennis@email.com <dennis@email.com>
2024-04-29 12:49:45 +0800

    modify to compile in Ubuntu

--- code/include/proto/imap/CImapProtoCmd.cpp
+++ code/include/proto/imap/CImapProtoCmd.cpp
@@ -4,6 +4,11 @@
 #include <stdio.h>
 namespace tiny_email
 {
+	#ifdef WINDOWS
+	#define STRICMP stricmp
+	#else
+	#define STRICMP strcasecmp
+	#endif
 	CImapProtoCmd CImapProtoCmd::FromString(const std::string& strValue)
 	{
 		std::size_t startIndex = 0;
@@ -31,39 +36,39 @@ namespace tiny_email
 
 		{
 			ImapCode_t imapCode = ImapCode_t::IMAP_CMD_UNKNOWN_CMD;
-			if (stricmp(strCmd.c_str(), "CAPABILITY") == 0)
+			if (STRICMP(strCmd.c_str(), "CAPABILITY") == 0)
 			{
 				imapCode = ImapCode_t::IMAP_CMD_CAPABILITY;
 			}
-			if (stricmp(strCmd.c_str(), "NOOP") == 0)
+			if (STRICMP(strCmd.c_str(), "NOOP") == 0)
 			{
 				imapCode = ImapCode_t::IMAP_CMD_NOOP;
 			}
-			if (stricmp(strCmd.c_str(), "SELECT") == 0)
+			if (STRICMP(strCmd.c_str(), "SELECT") == 0)
 			{
 				imapCode = ImapCode_t::IMAP_CMD_SELECT;
 			}
-			if (stricmp(strCmd.c_str(), "EXAMINE") == 0)
+			if (STRICMP(strCmd.c_str(), "EXAMINE") == 0)
 			{
 				imapCode = ImapCode_t::IMAP_CMD_EXAMINE;
 			}
-			if (stricmp(strCmd.c_str(), "SEARCH") == 0)
+			if (STRICMP(strCmd.c_str(), "SEARCH") == 0)
 			{
 				imapCode = ImapCode_t::IMAP_CMD_SEARCH;
 			}
-			if (stricmp(strCmd.c_str(), "FETCH") == 0)
+			if (STRICMP(strCmd.c_str(), "FETCH") == 0)
 			{
 				imapCode = ImapCode_t::IMAP_CMD_FETCH;
 			}
-			if (stricmp(strCmd.c_str(), "LIST") == 0)
+			if (STRICMP(strCmd.c_str(), "LIST") == 0)
 			{
 				imapCode = ImapCode_t::IMAP_CMD_LIST;
 			}
-			if (stricmp(strCmd.c_str(), "LOGOUT") == 0)
+			if (STRICMP(strCmd.c_str(), "LOGOUT") == 0)
 			{
 				imapCode = ImapCode_t::IMAP_CMD_LOGOUT;
 			}
-			if (stricmp(strCmd.c_str(), "LOGIN") == 0)
+			if (STRICMP(strCmd.c_str(), "LOGIN") == 0)
 			{
 				imapCode = ImapCode_t::IMAP_CMD_LOGIN;
 			}
