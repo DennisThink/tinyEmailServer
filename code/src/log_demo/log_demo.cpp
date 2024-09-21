9273ba30f86e508d928534532f094db5620ba95b
DennisThink-Windows <dennisthink@hotmail.com>
2024-09-21 15:03:00 +0800

    add some files

--- code/src/log_demo/log_demo.cpp
+++ code/src/log_demo/log_demo.cpp
@@ -2,34 +2,6 @@
 int main(int argc, char *argv[])
 {
     std::cout << argc << "  " << argv[0] << std::endl;
-    //{
-    //    auto logger = GetLogger();
-    //    LOG_DEBUG(logger, "INFO");
-    //    LOG_INFO(logger, "INFO");
-    //    LOG_WARN(logger, "INFO");
-    //    LOG_ERROR(logger, "INFO");
-    //    LOG_FATAL(logger, "INFO");
-    //}
-    //{
-    //    auto log2 = GetLogger();
-    //    LOG_DEBUG(log2, "INFO {}",1);
-    //    LOG_INFO(log2, "INFO {} {} ",2,"failed");
-    //    LOG_WARN(log2, "INFO {} {} {}",1,1.2,3);
-    //    LOG_ERROR(log2, "INFO");
-    //    LOG_FATAL(log2, "INFO");
-    //}
-    //{
-    //    std::cout << std::format("Hello {}!\n", "world");
-
-    //    std::string fmt;
-    //    for (int i{}; i != 3; ++i)
-    //    {
-    //        fmt += "{} "; // constructs the formatting string
-    //        std::cout << fmt << " : ";
-    //        std::cout << dyna_print(fmt, "alpha", 'Z', 3.14, "unused");
-    //        std::cout << '\n';
-    //    }
-    //}
     {
         std::string strFmt = "Hello";
         tiny_email::Debug("Debug Hello");
--- code/src/test_demo/pop3_proto_test.cpp
+++ code/src/test_demo/pop3_proto_test.cpp
@@ -5,6 +5,56 @@
 #include "CMemDataBase.h"
 #include <memory>
 #include <memory.h>
+
+TEST_CASE("pop3_proto_parse") {
+	{
+		std::string strCmdUser = "USER test\r\n";
+		tiny_email::CPop3ProtoReqCmd reqCmd;
+		tiny_email::PARSE_POP3_RESULT result = tiny_email::CPop3ProtoReqCmd::FromString(strCmdUser, reqCmd);
+		CHECK_EQ(result, tiny_email::PARSE_POP3_RESULT::PARSE_POP3_SUCCEED);
+		CHECK_EQ(reqCmd.GetCode(), tiny_email::POP3_CMD_t::POP3_CMD_USER_NAME);
+	}
+
+	{
+		std::string strCmdPassowrd = "PASS test\r\n";
+		tiny_email::CPop3ProtoReqCmd reqCmd;
+		tiny_email::PARSE_POP3_RESULT result = tiny_email::CPop3ProtoReqCmd::FromString(strCmdPassowrd, reqCmd);
+		CHECK_EQ(result, tiny_email::PARSE_POP3_RESULT::PARSE_POP3_SUCCEED);
+		CHECK_EQ(reqCmd.GetCode(), tiny_email::POP3_CMD_t::POP3_CMD_PASS_WORD);
+	}
+
+	{
+		std::string strCmdStat = "STAT\r\n";
+		tiny_email::CPop3ProtoReqCmd reqCmd;
+		tiny_email::PARSE_POP3_RESULT result = tiny_email::CPop3ProtoReqCmd::FromString(strCmdStat, reqCmd);
+		CHECK_EQ(result, tiny_email::PARSE_POP3_RESULT::PARSE_POP3_SUCCEED);
+		CHECK_EQ(reqCmd.GetCode(), tiny_email::POP3_CMD_t::POP3_CMD_STAT);
+	}
+
+	{
+		std::string strCmdList = "LIST\r\n";
+		tiny_email::CPop3ProtoReqCmd reqCmd;
+		tiny_email::PARSE_POP3_RESULT result = tiny_email::CPop3ProtoReqCmd::FromString(strCmdList, reqCmd);
+		CHECK_EQ(result, tiny_email::PARSE_POP3_RESULT::PARSE_POP3_SUCCEED);
+		CHECK_EQ(reqCmd.GetCode(), tiny_email::Pop3Code_t::POP3_SEND_USER_NAME);
+	}
+
+	{
+		std::string strCmdRETR = "RETR 1\r\n";
+		tiny_email::CPop3ProtoReqCmd reqCmd;
+		tiny_email::PARSE_POP3_RESULT result = tiny_email::CPop3ProtoReqCmd::FromString(strCmdRETR, reqCmd);
+		CHECK_EQ(result, tiny_email::PARSE_POP3_RESULT::PARSE_POP3_SUCCEED);
+		CHECK_EQ(reqCmd.GetCode(), tiny_email::Pop3Code_t::POP3_SEND_USER_NAME);
+	}
+
+	{
+		std::string strCmdQuit = "quit\r\n";
+		tiny_email::CPop3ProtoReqCmd reqCmd;
+		tiny_email::PARSE_POP3_RESULT result = tiny_email::CPop3ProtoReqCmd::FromString(strCmdQuit, reqCmd);
+		CHECK_EQ(result, tiny_email::PARSE_POP3_RESULT::PARSE_POP3_SUCCEED);
+		CHECK_EQ(reqCmd.GetCode(), tiny_email::Pop3Code_t::POP3_SEND_USER_NAME);
+	}
+}
 TEST_CASE("pop3_proto_test") {
 	std::string strDomain = "test.com";
 	auto db_ptr = std::make_shared<tiny_email::CMemDataBase>();
@@ -18,21 +68,21 @@ TEST_CASE("pop3_proto_test") {
 		std::string strCmdUser = "USER test\r\n";
 		CHECK(handler->OnClientReq(strCmdUser));
 		std::string strUserRsp = handler->GetResponse();
-		CHECK_EQ(strUserRsp, " +OK cpp mail user name\r\n");
+		CHECK_EQ(strUserRsp, "+OK cpp mail user name\r\n");
 	}
 
 	{
 		std::string strCmdPassowrd = "PASS test\r\n";
 		CHECK(handler->OnClientReq(strCmdPassowrd));
 		std::string strPassowordRsp = handler->GetResponse();
-		CHECK_EQ(strPassowordRsp, "");
+		CHECK_EQ(strPassowordRsp, "+OK 1 message(s) [102 byte(s)]\r\n");
 	}
 
 	{
 		std::string strCmdStat = "STAT\r\n";
 		CHECK(handler->OnClientReq(strCmdStat));
 		std::string strStatRsp = handler->GetResponse();
-		CHECK_EQ(strStatRsp, "");
+		CHECK_EQ(strStatRsp, "+OK 0\r\n");
 	}
 
 	{
@@ -46,13 +96,13 @@ TEST_CASE("pop3_proto_test") {
 		std::string strCmdRETR = "RETR 1\r\n";
 		CHECK(handler->OnClientReq(strCmdRETR));
 		std::string strRetrRsp = handler->GetResponse();
-		CHECK_EQ(strRetrRsp, "");
+		CHECK_EQ(strRetrRsp, "+OK 1 message(s) [102 byte(s)]\r\n");
 	}
 
 	{
 		std::string strCmdQuit = "quit\r\n";
-		CHECK(handler->OnClientReq(strCmdQuit));
+		CHECK_FALSE(handler->OnClientReq(strCmdQuit));
 		std::string strQuitRsp = handler->GetResponse();
-		CHECK_EQ(strQuitRsp, "");
+		CHECK_NE(strQuitRsp, "");
 	}
 }
